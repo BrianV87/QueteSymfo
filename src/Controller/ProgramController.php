32d459df2031a02245b9bf9769186c3d01892b46
BrianV87 <Brian.v87@hotmail.fr>
2023-05-23 12:55:23 +0200

    Mise en place de la recuperations des données avec doctrine-quete10

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,28 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProgramController extends AbstractController
 {
-    #[Route('/program', name: 'app_program')]
-    public function index(): Response
+    #[Route('/program/', name: 'program_index')]
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
         return $this->render('program/index.html.twig', [
-            'controller_name' => 'ProgramController',
+            'programs' => $programs,
         ]);
     }
 
-    #[Route('/program/{id}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'app_program_show')]
-    public function show(int $id): Response
+    #[Route('/program/{id}', name: 'program_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->find($id);
         return $this->render(
-            'program/index.html.twig',
+            'program/show.html.twig',
             [
-                'controller_name' => 'ProgramController',
-                'program_id' => $id
+                'program' => $program,
             ]
         );
     }
