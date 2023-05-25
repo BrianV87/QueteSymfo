b800c7638a604cbbe296a4ac86b79129f81231ae
BrianV87 <Brian.v87@hotmail.fr>
2023-05-25 13:03:58 +0200

    quete 14, gardez la FORM

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,18 +22,35 @@ class CategoryController extends AbstractController
             'categories' => $categories,
         ]);
     }
+    #[Route('/category/new', name: 'category_new')]
+    public function new(Request $request, CategoryRepository $categoryRepository) : Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $categoryRepository->save($category, true);
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
     #[Route('/category/{categoryName}', name: 'category_show')]
     public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository) : Response
     {
         $category = $categoryRepository->findOneBy(['name' => $categoryName]);
 
-        if (!$category) {
-        throw $this->createNotFoundException('La catégorie n\'existe pas');
-    }
+        if (!$category){
+            throw $this->createNotFoundException(
+                'No category with name :' . $categoryName . 'found in category table'
+            );
+        }
         $programs = $programRepository->findBy(
         ['category' => $category],
         [ 'id' => 'DESC'],
-        3
+        10
     );
 
         return $this->render('category/show.html.twig',[
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
 use App\Repository\ProgramRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +20,28 @@ class ProgramController extends AbstractController
         $programs = $programRepository->findAll();
         return $this->render('program/index.html.twig', [
             'programs' => $programs,
+        ]);
+    }
+    #[Route('/program/new', name: 'new')]
+    public function new(Request $request, ProgramRepository $programRepository): Response
+    {
+        $program = new Program();
+
+        // Create the form, linked with $category
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted())
+        {
+            $programRepository->save($program, true);
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        // Render the form
+
+        return $this->render('program/new.html.twig', [
+            'form' => $form,
         ]);
     }
 
